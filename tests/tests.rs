56082c1c0508ca75653d204d174612e69419bfdc
Chris Mumford <cmumford@cmumford.com>
2026-01-28 18:30:32 -0500

    Add test: test_rt_convert_ascii()

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,81 +1,13 @@
 use modular_bitfield_msb::prelude::*;
-use rds::{GroupType, GroupVersion, ProgramType, RtVariant};
+use rds::{GroupType, GroupVersion, ProgramType, RtVariant, rds_to_utf8_lossy};
 
 #[cfg(test)]
 
 mod tests {
     use super::*;
 
-    // #[derive(Specifier, Debug, PartialEq, Eq)]
-    // #[bits = 1]
-    // pub enum MyGroupVersion {
-    //     A = 0,
-    //     B = 1,
-    // }
-
-    // #[bitfield(bits = 5)]
-    // #[derive(Specifier, Default, Copy, Clone, PartialEq, Eq)]
-    // pub struct MyGroupType {
-    //     pub code: B4,                // Group type code.
-    //     pub version: MyGroupVersion, // Group version (A/B).
-    // }
-
-    // #[derive(Specifier, Debug, Default, Clone, PartialEq, Eq)]
-    // #[bits = 5]
-    // pub enum MyProgramType {
-    //     #[default]
-    //     None = 0,
-    //     News = 1,
-    //     Information = 2,
-    //     Sports = 3,
-    //     Talk = 4,
-    //     Rock = 5,
-    //     ClassicRock = 6,
-    //     AdultHits = 7,
-    //     SoftRock = 8,
-    //     Top40 = 9,
-    //     Country = 10,
-    //     Oldies = 11,
-    //     Soft = 12,
-    //     Nostalgia = 13,
-    //     Jazz = 14,
-    //     Classical = 15,
-    //     RhythmAndBlues = 16,
-    //     SoftRhythmAndBlues = 17,
-    //     ForeignLanguage = 18,
-    //     ReligiousMusic = 19,
-    //     ReligiousTalk = 20,
-    //     Personality = 21,
-    //     Public = 22,
-    //     College = 23,
-    //     Unnasigned1 = 24,
-    //     Unnasigned2 = 25,
-    //     Unnasigned3 = 26,
-    //     Unnasigned4 = 27,
-    //     Unnasigned5 = 28,
-    //     Weather = 29,
-    //     EmergencyTest = 30,
-    //     Emergency = 31,
-    // }
-
-    // #[derive(Specifier, Debug, Default, Clone, Copy, PartialEq, Eq)]
-    // #[bits = 1]
-    // pub enum MyRtVariant {
-    //     #[default]
-    //     A,
-    //     B,
-    // }
-
-    #[bitfield(filled = false)]
-    #[derive(BitfieldSpecifier, PartialEq, Eq, Copy, Clone)]
-    struct BlockBCommon {
-        group_type: GroupType,     // Group type (code + version).
-        traffic_program: bool,     // TP bit.
-        program_type: ProgramType, // PTY: Program type.
-    }
-
     #[bitfield]
-    #[cfg_attr(not(feature = "unknown"), repr(u16))]
+    #[repr(u16)]
     struct GroupType2BlockB {
         group_type: GroupType,     // Group type (code + version).
         traffic_program: bool,     // TP bit.
@@ -99,5 +31,17 @@ mod tests {
         assert_eq!(block.program_type(), ProgramType::ClassicRock);
         assert_eq!(block.text_flag(), RtVariant::B);
         assert_eq!(block.text_segment_addr(), 10);
+    }
+
+    #[test]
+    fn test_rt_convert_ascii() {
+        assert_eq!(
+            rds_to_utf8_lossy(
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:{}[]();!\"*+-'./%&"
+                    .as_bytes()
+            ),
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:{}[]();!\"*+-'./%&"
+                .to_string()
+        );
     }
 }
