9b7b600d8988f1f59b81a34555a5fdde35df8221
Chris Mumford <cmumford@cmumford.com>
2026-01-31 13:22:57 -0500

    Replace PsPrivate with TextProb for Radiotext

--- src/radiotext.rs
+++ src/radiotext.rs
@@ -1,3 +1,4 @@
+use crate::text_prob::TextProb;
 use modular_bitfield_msb::prelude::*;
 
 pub const MAX_RADIOTEXT_LEN: usize = 64;
@@ -27,35 +28,18 @@ const TABLE2: [char; 256] = [
 'ã', 'å', 'æ', 'œ', 'ŵ', 'ý', 'õ', 'ø' , 'þ', 'ŋ', 'ŕ', 'ć', 'ś' , 'ź', 'ŧ', ' '];
 
 /// Radiotext (RT) decoding state for one variant (A or B)
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Radiotext {
     /// Final decoded text.
     pub display: [u8; MAX_RADIOTEXT_LEN],
-    pvt: RadioTextPvt,
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-struct RadioTextPvt {
-    hi_prob: [u8; MAX_RADIOTEXT_LEN], // Temporary Radiotext (high probability).
-    lo_prob: [u8; MAX_RADIOTEXT_LEN], // Temporary Radiotext (low probability).
-    hi_prob_cnt: [u8; MAX_RADIOTEXT_LEN], // Hit count of high probability Radiotext.
-}
-
-impl Default for RadioTextPvt {
-    fn default() -> Self {
-        Self {
-            hi_prob: [BLANK_CHAR; MAX_RADIOTEXT_LEN],
-            lo_prob: [BLANK_CHAR; MAX_RADIOTEXT_LEN],
-            hi_prob_cnt: [BLANK_CHAR; MAX_RADIOTEXT_LEN],
-        }
-    }
+    pvt: TextProb<MAX_RADIOTEXT_LEN>,
 }
 
 impl Default for Radiotext {
     fn default() -> Self {
         Self {
             display: [BLANK_CHAR; MAX_RADIOTEXT_LEN],
-            pvt: RadioTextPvt::default(),
+            pvt: TextProb::default(),
         }
     }
 }
@@ -69,7 +53,7 @@ pub enum RtVariant {
     B,
 }
 
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct RtData {
     pub a: Radiotext,         // RT A text.
     pub b: Radiotext,         // RT B text.
@@ -83,84 +67,6 @@ pub fn rds_to_utf8_lossy(bytes: &[u8]) -> String {
             _ => TABLE2[b as usize],
         })
         .collect()
-}
-
-impl RadioTextPvt {
-    fn update_rt_advance_ch(&mut self, idx: usize, byte: u8) -> bool {
-        let mut text_changing = false;
-        // The new byte matches the high probability byte.
-        if self.hi_prob[idx] == byte {
-            if self.hi_prob_cnt[idx] < RT_VALIDATE_LIMIT {
-                self.hi_prob_cnt[idx] += 1;
-            } else {
-                // we have received this byte enough to max out our counter and push
-                // it into the low probability array as well.
-                self.hi_prob_cnt[idx] = RT_VALIDATE_LIMIT;
-                self.lo_prob[idx] = byte;
-            }
-        } else if self.lo_prob[idx] == byte {
-            // The new byte is a match with the low probability byte. Swap them,
-            // reset the counter and flag the text as in transition. Note that the
-            // counter for this character goes higher than the validation limit
-            // because it will get knocked down later.
-            if self.hi_prob_cnt[idx] >= RT_VALIDATE_LIMIT {
-                text_changing = true;
-                self.hi_prob_cnt[idx] = RT_VALIDATE_LIMIT + 1;
-            } else {
-                self.hi_prob_cnt[idx] = RT_VALIDATE_LIMIT;
-            }
-            self.lo_prob[idx] = self.hi_prob[idx];
-            self.hi_prob[idx] = byte;
-        } else if !self.hi_prob_cnt[idx] == 0 {
-            // The new byte is replacing an empty byte in the high proability array.
-            self.hi_prob[idx] = byte;
-            self.hi_prob_cnt[idx] = 1;
-        } else {
-            // The new byte doesn't match anything, put it in the low probability
-            // array.
-            self.lo_prob[idx] = byte;
-        }
-        text_changing
-    }
-
-    /// The advanced implementation of the Radiotext update.
-    ///
-    /// This implementation of the Radiotext update attempts to further error
-    /// correct the data by making sure that the data has been identical for
-    /// multiple receptions of each byte.
-    pub fn update_rt_advance(&mut self, addr: usize, byte: &[Option<[u8; 2]>]) {
-        let mut text_changing = false; // Indicates if the Radiotext is changing.
-        let mut idx = addr;
-
-        let mut add_pair = |pair: &Option<[u8; 2]>| {
-            if pair.is_none() {
-                return;
-            }
-            for ch in pair.unwrap() {
-                if self.update_rt_advance_ch(idx, ch) {
-                    text_changing = true;
-                }
-                idx += 1;
-            }
-        };
-
-        add_pair(&byte[0]);
-        if byte.len() > 1 {
-            add_pair(&byte[1]);
-        }
-
-        if !text_changing {
-            return;
-        }
-
-        // When the text is changing, decrement the count for all characters to
-        // prevent displaying part of a message that is in transition.
-        for i in 0..self.hi_prob_cnt.len() {
-            if self.hi_prob_cnt[i] > 1 {
-                self.hi_prob_cnt[i] -= 1;
-            }
-        }
-    }
 }
 
 impl Radiotext {
@@ -198,7 +104,37 @@ impl Radiotext {
     }
 
     pub fn update_rt_advance(&mut self, addr: usize, byte: &[Option<[u8; 2]>]) {
-        self.pvt.update_rt_advance(addr, byte)
+        let mut text_changing = false; // Indicates if the Radiotext is changing.
+        let mut idx = addr;
+
+        let mut add_pair = |pair: &Option<[u8; 2]>| {
+            if pair.is_none() {
+                return;
+            }
+            for ch in pair.unwrap() {
+                if self.pvt.update(idx, ch) {
+                    text_changing = true;
+                }
+                idx += 1;
+            }
+        };
+
+        add_pair(&byte[0]);
+        if byte.len() > 1 {
+            add_pair(&byte[1]);
+        }
+
+        if !text_changing {
+            return;
+        }
+
+        // When the text is changing, decrement the count for all characters to
+        // prevent displaying part of a message that is in transition.
+        for i in 0..self.pvt.hi_prob_cnt.len() {
+            if self.pvt.hi_prob_cnt[i] > 1 {
+                self.pvt.hi_prob_cnt[i] -= 1;
+            }
+        }
     }
 
     pub fn bump_rt_validation_count(&mut self) {
