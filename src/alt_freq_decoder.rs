8717d1b27dbcd0af7aa8fd43617630e4964953b1
Chris Mumford <cmumford@cmumford.com>
2026-02-07 15:47:37 -0500

    Get AF method B decoding working

--- src/alt_freq_decoder.rs
+++ src/alt_freq_decoder.rs
@@ -16,6 +16,7 @@ enum CodeType {
     Frequency,    // A UHF frequency value.
     AltFreqCount, // Number of AF's to follow.
     LfMfFollows,  // Next entry is a LF/MF freq.
+    Filler,       // A no-op.
 }
 
 enum FreqBand {
@@ -28,6 +29,7 @@ enum FreqBand {
 fn categorize_uhf_code(code: u8) -> CodeType {
     match code {
         1..=204 => CodeType::Frequency,
+        205 => CodeType::Filler,
         224..=249 => CodeType::AltFreqCount,
         250 => CodeType::LfMfFollows,
         _ => CodeType::Unassigned,
@@ -62,6 +64,8 @@ fn get_uhf_frequency(idx: u8) -> u32 {
 pub enum DecodeError {
     #[error("Use of invalid code")]
     InvalidCode,
+    #[error("Invalid frequency count")]
+    InvalidFreqCount,
 }
 
 #[derive(Debug, Default, Clone, PartialEq)]
@@ -77,6 +81,7 @@ pub struct AfDecoder {
     awaiting_freq_cnt: u8,          // Number of expected frequencies in table.
     next_freq_is_lf_mf: bool,       // Is the next frquency LF/MF?
     first_freq_code: u8,            // The first frequency code in the table.
+    first_freq_in_table: bool,      // Has first frequency been added to the table?
     encoding_method: EcodingMethod, // Table encoding method.
 }
 
@@ -85,68 +90,97 @@ impl AfDecoder {
         self.awaiting_freq_cnt = 0;
         self.next_freq_is_lf_mf = false;
         self.first_freq_code = 0;
+        self.first_freq_in_table = false;
         self.encoding_method = EcodingMethod::Unknown;
     }
 
-    fn decode_freq_code(&mut self, code: u8, table: &mut AfTable) -> Result<(), DecodeError> {
-        let code_type = categorize_uhf_code(code);
-        if code_type == CodeType::Unassigned {
-            return Err(DecodeError::InvalidCode);
-        }
-        if self.awaiting_freq_cnt == 0 {
-            if code_type != CodeType::AltFreqCount {
-                // Not an error because decoding may have started after the
-                // AF table start. Waiting until the next table start which is
-                // indicated by the frequency count.
-                return Ok(());
-            }
-            self.awaiting_freq_cnt = decode_freq_cnt(code);
-            return Ok(());
-        }
-        if self.next_freq_is_lf_mf {
-            self.next_freq_is_lf_mf = false;
-            if code_type != CodeType::Frequency {
-                self.reset();
-                return Err(DecodeError::InvalidCode);
-            }
-            let _ = table.add(&Freq {
-                frequency: get_lf_mf_frequency(code),
-                freq_type: FreqType::SameProgram,
-            });
-            return Ok(());
-        }
-        if code_type == CodeType::LfMfFollows {
-            self.next_freq_is_lf_mf = true;
-            return Ok(());
-        }
-        assert!(code_type == CodeType::Frequency);
-        if self.first_freq_code == 0 {
-            self.first_freq_code = code;
-        }
+    fn write_first_freq_to_table(&mut self, table: &mut AfTable) {
+        assert!(!self.first_freq_in_table);
+        assert_ne!(self.first_freq_code, 0);
+        self.first_freq_in_table = true;
         let _ = table.add(&Freq {
-            frequency: get_uhf_frequency(code),
+            frequency: get_uhf_frequency(self.first_freq_code),
             freq_type: FreqType::SameProgram,
         });
+    }
 
+    // Only called when we know encoding is method A.
+    fn decode_for_method_a_code(
+        &mut self,
+        code: u8,
+        table: &mut AfTable,
+    ) -> Result<(), DecodeError> {
+        if !self.first_freq_in_table && self.first_freq_code != 0 {
+            self.write_first_freq_to_table(table);
+        }
+        match categorize_uhf_code(code) {
+            CodeType::Unassigned => {
+                return Err(DecodeError::InvalidCode);
+            }
+            CodeType::AltFreqCount => {
+                return Err(DecodeError::InvalidCode);
+            }
+            CodeType::LfMfFollows => {
+                if self.next_freq_is_lf_mf {
+                    return Err(DecodeError::InvalidCode);
+                }
+                self.next_freq_is_lf_mf = true;
+                return Ok(());
+            }
+            CodeType::Frequency => {
+                if self.awaiting_freq_cnt == 0 {
+                    return Err(DecodeError::InvalidFreqCount);
+                }
+                self.awaiting_freq_cnt -= 1;
+                let freq: u32;
+                if self.next_freq_is_lf_mf {
+                    self.next_freq_is_lf_mf = false;
+                    freq = get_lf_mf_frequency(code);
+                } else {
+                    freq = get_uhf_frequency(code);
+                }
+                let _ = table.add(&Freq {
+                    frequency: freq,
+                    freq_type: FreqType::SameProgram,
+                });
+            }
+            CodeType::Filler => (),
+        }
         Ok(())
     }
 
+    // Only called when we know encoding is method A.
+    // and only for blocks 2..n and not the first block.
+    fn decode_for_method_a(
+        &mut self,
+        code_pair: [u8; 2],
+        table: &mut AfTable,
+    ) -> Result<(), DecodeError> {
+        let _ = self.decode_for_method_a_code(code_pair[0], table);
+        let _ = self.decode_for_method_a_code(code_pair[1], table);
+        Ok(())
+    }
+
+    // Only called when we know encoding is method B.
+    // and only for blocks 2..n and not the first block.
     fn decode_for_method_b(
         &mut self,
         code_pair: [u8; 2],
         table: &mut AfTable,
     ) -> Result<(), DecodeError> {
         let freq_type = if code_pair[0] < code_pair[1] {
-            FreqType::RegionalVariant
-        } else {
             FreqType::SameProgram
+        } else {
+            FreqType::RegionalVariant
         };
         if code_pair[0] == self.first_freq_code {
+            assert_ne!(code_pair[1], self.first_freq_code);
             let _ = table.add(&Freq {
                 frequency: get_uhf_frequency(code_pair[1]),
                 freq_type: freq_type,
             });
         } else if code_pair[1] == self.first_freq_code {
+            assert_ne!(code_pair[0], self.first_freq_code);
             let _ = table.add(&Freq {
                 frequency: get_uhf_frequency(code_pair[0]),
                 freq_type: freq_type,
@@ -155,6 +189,58 @@ impl AfDecoder {
             assert!(false, "Freq does not match tuned freq");
         }
         Ok(())
+    }
+
+    fn decode_for_unknown_method(
+        &mut self,
+        code_pair: [u8; 2],
+        table: &mut AfTable,
+    ) -> Result<(), DecodeError> {
+        let ct1 = categorize_uhf_code(code_pair[0]);
+        let ct2 = categorize_uhf_code(code_pair[1]);
+
+        if self.first_freq_code == 0 {
+            assert_eq!(self.first_freq_in_table, false);
+            match ct1 {
+                CodeType::Unassigned => return Err(DecodeError::InvalidCode),
+                CodeType::AltFreqCount => {
+                    self.awaiting_freq_cnt = decode_freq_cnt(code_pair[0]);
+                    if ct2 == CodeType::Frequency {
+                        // Could be Method A or B - don't know yet. When the next block
+                        // is decoded that will be determined.
+                        self.first_freq_code = code_pair[1];
+                        if self.awaiting_freq_cnt < 3 {
+                            self.write_first_freq_to_table(table);
+                        }
+                        return Ok(());
+                    } else {
+                        // This could be a LF/MF code (or other), so send to method A decoder.
+                        self.encoding_method = EcodingMethod::MethodA;
+                        return self.decode_for_method_a_code(code_pair[1], table);
+                    }
+                }
+                _ => {
+                    // No freq count. Probably started decoding in a table stream
+                    // so wait for next freq count code.
+                    return Ok(());
+                }
+            }
+        }
+
+        // If here then processing block 2..n.
+        if ct1 != CodeType::Frequency || ct2 != CodeType::Frequency {
+            self.encoding_method = EcodingMethod::MethodA;
+        } else if code_pair[0] == self.first_freq_code || code_pair[1] == self.first_freq_code {
+            self.encoding_method = EcodingMethod::MethodB;
+        } else {
+            self.encoding_method = EcodingMethod::MethodA;
+        }
+
+        match self.encoding_method {
+            EcodingMethod::MethodA => self.decode_for_method_a(code_pair, table),
+            EcodingMethod::MethodB => self.decode_for_method_b(code_pair, table),
+            _ => panic!("Shouldn't get here"),
+        }
     }
 
     // Decode a C block of frequencies or control codes. If none was received,
@@ -169,249 +255,13 @@ impl AfDecoder {
             return Ok(());
         }
         let code_pair = block_c.unwrap().to_be_bytes();
-        if self.encoding_method == EcodingMethod::Unknown && self.first_freq_code != 0 {
-            if code_pair[0] == self.first_freq_code || code_pair[1] == self.first_freq_code {
-                self.encoding_method = EcodingMethod::MethodB;
-            } else {
-                self.encoding_method = EcodingMethod::MethodA;
-            }
+        match self.encoding_method {
+            EcodingMethod::MethodA => self.decode_for_method_a(code_pair, table),
+            EcodingMethod::MethodB => self.decode_for_method_b(code_pair, table),
+            EcodingMethod::Unknown => self.decode_for_unknown_method(code_pair, table),
         }
-        if self.encoding_method == EcodingMethod::MethodB {
-            return self.decode_for_method_b(code_pair, table);
-        }
-        // Either unknown or method A
-        let _ = self.decode_freq_code(code_pair[0], table);
-        let _ = self.decode_freq_code(code_pair[1], table);
-
-        Ok(())
     }
 }
 
 #[cfg(test)]
-
-mod tests {
-    use crate::alt_freq_decoder::{
-        AfDecoder, decode_freq_cnt, get_lf_mf_frequency, get_uhf_frequency,
-    };
-    use crate::alt_freq_table::{AfTable, Freq, FreqType};
-
-    #[test]
-    fn test_get_lf_mf_frequency() {
-        assert_eq!(get_lf_mf_frequency(1), 153_000);
-        assert_eq!(get_lf_mf_frequency(15), 279_000);
-        assert_eq!(get_lf_mf_frequency(16), 531_000);
-        assert_eq!(get_lf_mf_frequency(135), 1_602_000);
-    }
-
-    #[test]
-    fn test_get_uhf_frequency() {
-        assert_eq!(get_uhf_frequency(1), 87_600_000);
-        assert_eq!(get_uhf_frequency(2), 87_700_000);
-        assert_eq!(get_uhf_frequency(204), 107_900_000);
-    }
-
-    #[test]
-    fn test_decode_freq_cnt() {
-        assert_eq!(decode_freq_cnt(224), 0);
-        assert_eq!(decode_freq_cnt(225), 1);
-        assert_eq!(decode_freq_cnt(249), 25);
-    }
-
-    #[test]
-    fn test_decoder_one_freq() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let result = decoder.decode_freq_block(Some(0xE1_01), &mut table);
-        assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        assert_eq!(table.iter().count(), 1);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [Freq {
-                frequency: 87_600_000,
-                freq_type: FreqType::SameProgram
-            }]
-        );
-    }
-
-    // test to very scenario from Example A in RBDS Specification section 3.2.1.6.3.
-    #[test]
-    fn test_decoder_method_a_example_a() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let blocks = [0xE5_01, 0x02_03, 0x04_05];
-        for block in blocks {
-            let result = decoder.decode_freq_block(Some(block), &mut table);
-            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        }
-        assert_eq!(table.iter().count(), 5);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [
-                Freq {
-                    frequency: 87_600_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_700_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_800_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_900_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 88_000_000,
-                    freq_type: FreqType::SameProgram
-                },
-            ]
-        );
-    }
-
-    // test to very scenario from Example B in RBDS Specification section 3.2.1.6.3.
-    #[test]
-    fn test_decoder_method_a_example_b() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let blocks = [0xE5_01, 0x02_03, 0x04_CE];
-        for block in blocks {
-            let result = decoder.decode_freq_block(Some(block), &mut table);
-            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        }
-        assert_eq!(table.iter().count(), 4);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [
-                Freq {
-                    frequency: 87_600_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_700_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_800_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_900_000,
-                    freq_type: FreqType::SameProgram
-                },
-            ]
-        );
-    }
-
-    // test to very scenario from Example C in RBDS Specification section 3.2.1.6.3.
-    #[test]
-    fn test_decoder_method_a_example_c() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let blocks = [0xE5_01, 0x02_03, 0xFA_10];
-        for block in blocks {
-            let result = decoder.decode_freq_block(Some(block), &mut table);
-            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        }
-        assert_eq!(table.iter().count(), 4);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [
-                Freq {
-                    frequency: 87_600_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_700_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 87_800_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 531_000,
-                    freq_type: FreqType::SameProgram
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn test_decoder_method_b_example_1() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let blocks = [0xEC_12, 0x12_78, 0x12_8E, 0x0D_12, 0x97_12, 0x12_0F];
-        for block in blocks {
-            let result = decoder.decode_freq_block(Some(block), &mut table);
-            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        }
-        assert_eq!(table.iter().count(), 5);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [
-                Freq {
-                    frequency: 99_500_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 101_700_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 88_800_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 102_600_000,
-                    freq_type: FreqType::RegionalVariant
-                },
-                Freq {
-                    frequency: 89_000_000,
-                    freq_type: FreqType::RegionalVariant
-                },
-            ]
-        );
-    }
-
-    #[test]
-    fn test_decoder_method_b_example_2() {
-        let mut table = AfTable::default();
-        let mut decoder = AfDecoder::default();
-        let blocks = [0xEA_78, 0x12_78, 0x78_86, 0xAD_78, 0x78_10];
-        for block in blocks {
-            let result = decoder.decode_freq_block(Some(block), &mut table);
-            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
-        }
-        assert_eq!(table.iter().count(), 5);
-        let actual: Vec<_> = table.iter().copied().collect();
-        assert_eq!(
-            actual,
-            [
-                Freq {
-                    frequency: 89_300_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 100_900_000,
-                    freq_type: FreqType::SameProgram
-                },
-                Freq {
-                    frequency: 104_800_000,
-                    freq_type: FreqType::RegionalVariant
-                },
-                Freq {
-                    frequency: 89_100_000,
-                    freq_type: FreqType::RegionalVariant
-                },
-            ]
-        );
-    }
-}
+mod tests;
---
+++ src/alt_freq_decoder/tests.rs
@@ -0,0 +1,231 @@
+#[cfg(test)]
+
+mod tests {
+    use crate::alt_freq_decoder::{
+        AfDecoder, decode_freq_cnt, get_lf_mf_frequency, get_uhf_frequency,
+    };
+    use crate::alt_freq_table::{AfTable, Freq, FreqType};
+
+    #[test]
+    fn test_get_lf_mf_frequency() {
+        assert_eq!(get_lf_mf_frequency(1), 153_000);
+        assert_eq!(get_lf_mf_frequency(15), 279_000);
+        assert_eq!(get_lf_mf_frequency(16), 531_000);
+        assert_eq!(get_lf_mf_frequency(135), 1_602_000);
+    }
+
+    #[test]
+    fn test_get_uhf_frequency() {
+        assert_eq!(get_uhf_frequency(1), 87_600_000);
+        assert_eq!(get_uhf_frequency(2), 87_700_000);
+        assert_eq!(get_uhf_frequency(204), 107_900_000);
+    }
+
+    #[test]
+    fn test_decode_freq_cnt() {
+        assert_eq!(decode_freq_cnt(224), 0);
+        assert_eq!(decode_freq_cnt(225), 1);
+        assert_eq!(decode_freq_cnt(249), 25);
+    }
+
+    #[test]
+    fn test_decoder_one_freq() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let result = decoder.decode_freq_block(Some(0xE1_01), &mut table);
+        assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        assert_eq!(table.iter().count(), 1);
+        let actual: Vec<_> = table.iter().copied().collect();
+        assert_eq!(
+            actual,
+            [Freq {
+                frequency: 87_600_000,
+                freq_type: FreqType::SameProgram
+            }]
+        );
+    }
+
+    // test to very scenario from Example A in RBDS Specification section 3.2.1.6.3.
+    #[test]
+    fn test_decoder_method_a_example_a() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let blocks = [0xE5_01, 0x02_03, 0x04_05];
+        for block in blocks {
+            let result = decoder.decode_freq_block(Some(block), &mut table);
+            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        }
+        assert_eq!(table.iter().count(), 5);
+        let actual: Vec<_> = table.iter().copied().collect();
+        assert_eq!(
+            actual,
+            [
+                Freq {
+                    frequency: 87_600_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_700_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_800_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_900_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 88_000_000,
+                    freq_type: FreqType::SameProgram
+                },
+            ]
+        );
+    }
+
+    // test to very scenario from Example B in RBDS Specification section 3.2.1.6.3.
+    #[test]
+    fn test_decoder_method_a_example_b() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let blocks = [0xE5_01, 0x02_03, 0x04_CE];
+        for block in blocks {
+            let result = decoder.decode_freq_block(Some(block), &mut table);
+            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        }
+        assert_eq!(table.iter().count(), 4);
+        let actual: Vec<_> = table.iter().copied().collect();
+        assert_eq!(
+            actual,
+            [
+                Freq {
+                    frequency: 87_600_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_700_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_800_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_900_000,
+                    freq_type: FreqType::SameProgram
+                },
+            ]
+        );
+    }
+
+    // test to very scenario from Example C in RBDS Specification section 3.2.1.6.3.
+    #[test]
+    fn test_decoder_method_a_example_c() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let blocks = [0xE5_01, 0x02_03, 0xFA_10];
+        for block in blocks {
+            let result = decoder.decode_freq_block(Some(block), &mut table);
+            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        }
+        assert_eq!(table.iter().count(), 4);
+        let actual: Vec<_> = table.iter().copied().collect();
+        assert_eq!(
+            actual,
+            [
+                Freq {
+                    frequency: 87_600_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_700_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 87_800_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 531_000,
+                    freq_type: FreqType::SameProgram
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_decoder_method_b_example_1() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let blocks = [0xEC_12, 0x12_78, 0x12_8E, 0x0D_12, 0x97_12, 0x12_0F];
+        for block in blocks {
+            let result = decoder.decode_freq_block(Some(block), &mut table);
+            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        }
+        assert_eq!(table.iter().count(), 5);
+        let mut actual: Vec<_> = table.iter().copied().collect();
+        actual.sort_by_key(|f| f.frequency);
+        assert_eq!(
+            actual,
+            [
+                Freq {
+                    frequency: 88_800_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 89_000_000,
+                    freq_type: FreqType::RegionalVariant
+                },
+                Freq {
+                    frequency: 99_500_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 101_700_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 102_600_000,
+                    freq_type: FreqType::RegionalVariant
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_decoder_method_b_example_2() {
+        let mut table = AfTable::default();
+        let mut decoder = AfDecoder::default();
+        let blocks = [0xEA_78, 0x12_78, 0x78_86, 0xAD_78, 0x78_10];
+        for block in blocks {
+            let result = decoder.decode_freq_block(Some(block), &mut table);
+            assert!(result.is_ok(), "Expected Ok, got {:?}", result);
+        }
+        assert_eq!(table.iter().count(), 4);
+        let mut actual: Vec<_> = table.iter().copied().collect();
+        actual.sort_by_key(|f| f.frequency);
+
+        assert_eq!(
+            actual,
+            [
+                Freq {
+                    frequency: 89_100_000,
+                    freq_type: FreqType::RegionalVariant
+                },
+                Freq {
+                    frequency: 89_300_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 100_900_000,
+                    freq_type: FreqType::SameProgram
+                },
+                Freq {
+                    frequency: 104_800_000,
+                    freq_type: FreqType::RegionalVariant
+                },
+            ]
+        );
+    }
+}
